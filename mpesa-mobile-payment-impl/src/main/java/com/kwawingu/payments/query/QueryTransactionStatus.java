e8b2607aa7f215dd938e2b4ce22210fa7c739aaf
colspan <collin@collindigm.com>
2023-11-16 12:39:17 +0300

    Implemented a Query Transaction Status

---
+++ mpesa-mobile-payment-impl/src/main/java/com/kwawingu/payments/query/QueryTransactionStatus.java
@@ -0,0 +1,52 @@
+package com.kwawingu.payments.query;
+
+import com.kwawingu.payments.ApiEndpoint;
+import com.kwawingu.payments.Service;
+import com.kwawingu.payments.c2b.CustomerToBusinessTransaction;
+import com.kwawingu.payments.client.MpesaHttpClient;
+import com.kwawingu.payments.session.keys.MpesaEncryptedSessionKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+public class QueryTransactionStatus {
+    private static final Logger LOG = LoggerFactory.getLogger(CustomerToBusinessTransaction.class);
+
+    private final MpesaHttpClient mpesaHttpClientClient;
+    private final ApiEndpoint apiEndpoint;
+    private final MpesaEncryptedSessionKey encryptedSessionKey;
+
+    public QueryTransactionStatus(ApiEndpoint apiEndpoint, MpesaEncryptedSessionKey encryptedSessionKey) {
+        this.apiEndpoint = apiEndpoint;
+        this.encryptedSessionKey = encryptedSessionKey;
+        this.mpesaHttpClientClient = new MpesaHttpClient();
+    }
+
+    public String queryTransaction() {
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("Origin", "*");
+        encryptedSessionKey.insertAuthorizationHeader(headers);
+
+        URI uri = apiEndpoint.getUrl(Service.QUERY_TRANSACTION);
+        HttpResponse<String> response = null;
+
+        try {
+            response = mpesaHttpClientClient.QueryTransactionStatus(headers,
+                    HttpRequest.BodyPublishers.ofString(""),
+                    uri
+                    );
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return response.body();
+    }
+}
